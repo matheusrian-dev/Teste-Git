c3d60811eb3b562ec9033983797e15b84b237e22
fellipeotavionunes <pipito321@outlook.com>
2019-03-13 15:23:47 -0300

    iniciando o prejeto de teste

---
+++ App01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("");
+            Console.WriteLine("===============================================");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("                       Olá Mundo               ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("");
+            Console.WriteLine("===============================================");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
